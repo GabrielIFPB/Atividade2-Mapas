925c98520c65db4c1022368b85077b076a310f2a
gabriel <jeronimogbariel@gmail.com>
2020-01-06 21:34:09 -0300

    atividade 3 concluído - mas acho que não é assim, pós não entendi muito bem a questão

---
+++ src/exer3/GerarSenha.java
@@ -0,0 +1,22 @@
+package exer3;
+
+public class GerarSenha {
+	private String senha;
+	
+	public String gerarSenha(int qtd) {
+		++qtd;
+		if (qtd < 10)
+			this.setSenha( "CX0"+qtd );
+		else
+			this.setSenha( "CX"+qtd );
+		return this.getSenha();
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+}
